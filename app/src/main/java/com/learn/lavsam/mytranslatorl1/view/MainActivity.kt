0fcf62d176df90b2676ae20456500a71f9d0be5a
lavsam-dev <lavsam@gmail.com>
2021-08-29 17:13:01 +0400

    Созданы data classes and layouts

--- app/src/main/java/com/learn/lavsam/mytranslatorl1/view/MainActivity.kt
+++ app/src/main/java/com/learn/lavsam/mytranslatorl1/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.learn.lavsam.mytranslatorl1
+package com.learn.lavsam.mytranslatorl1.view
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import com.learn.lavsam.mytranslatorl1.R
 import com.learn.lavsam.mytranslatorl1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
